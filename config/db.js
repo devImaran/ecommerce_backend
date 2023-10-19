03d4e956b94ac31f922addd1fec46e920f706766
unknown <aimraan204@gmail.com>
2023-10-20 02:48:08 +0530

    db config checnged

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 require('dotenv').config()
 
 const dbConnect = async () =>{
-    await mongoose.connect(process.env.MONGO_DB_URI)
+    await mongoose.connect(process.env.MONGODB_URI)
     console.log("DB connected")
 }
 
