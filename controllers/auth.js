7220d14c214e5498fda795200009fdfd7e8670f5
unknown <aimraan204@gmail.com>
2023-10-31 22:04:59 +0530

    dep check

--- controllers/auth.js
+++ controllers/auth.js
@@ -28,22 +28,27 @@ const register = async (req, res, next) => {
 
 const login = async (req, res, next) => {
     try {
+        console.log('i am iside login')
         const { email, password } = req.body
 
         //check email exists or not 
         const isValidUser = await userModel.findOne({ email });
         if (!isValidUser) return next({ status: 400, message: 'Invalid Email!', errorOn: 'email' });
 
+        console.log('user found')
         //compare the password
         const hasCorrectPassword = bcrypt.compareSync(password, isValidUser.password);
         if (!hasCorrectPassword) return next({ status: 400, message: 'Wrong password!', errorOn: 'password' });
 
+
+        console.log('generating token')
         //generate token
         const { _id, name, email: userEmail, phoneNumber, profilePic, role } = isValidUser
 
         const token = jwt.sign({ _id, name, email, phoneNumber, profilePic, role }, process.env.TOKEN_SECRET_KEY, { expiresIn: '48h' })
         return res.json(response(200, "LoggedIn successfully!", { token }))
     } catch (err) {
+        console.log(err)
         return next(err)
     }
 }
